59bfa0d09189b35064a7e47327179dd684962fbd
laisamarcelino <laisamsr12@gmail.com>
2024-12-03 18:49:38 -0300

    ..

---
+++ sem_uso/especial.h
@@ -0,0 +1,13 @@
+#ifndef __ESPECIAL__
+#define __ESPECIAL__
+
+typedef struct {
+    unsigned char timer; // Cadência de tiro
+    unsigned char duracao; // Duração do ataque especial
+    unsigned char dano;
+    //balas *balas;
+
+} especial;
+
+
+#endif
